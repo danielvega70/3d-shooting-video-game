4f04796e66566b6998bde090b446447a04d4374e
danielvega70 <80554408+danielvega70@users.noreply.github.com>
2023-03-17 11:37:50 -0500

    Create walking physics.c

---
+++ walking physics.c
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    private Rigidbody rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0f, vertical) * speed;
+        rb.AddForce(movement, ForceMode.Impulse);
+    }
+}
